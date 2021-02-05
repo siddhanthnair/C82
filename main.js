38c07919c92ef2528368a109fee159130f48736d
siddhanthnair <72081663+siddhanthnair@users.noreply.github.com>
2021-02-05 19:46:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+canvas=document.getElementById('myCanvas');
+ctx=canvas.getContext("2d");
+canvas.addEventListener("mousedown",mymousedown);
+var mouseevent;
+function mymousedown(e){
+    mouseevent="mousedown";
+}
+canvas.addEventListener("mouseup",mymouseup);
+function mymouseup(e){
+    mouseevent="mouseup";
+}
+canvas.addEventListener("mouseleave",mymouseleave);
+function mymouseleave(e){
+    mouseevent="mouseleave";
+}
+canvas.addEventListener("mousemove",mymousemove);
+function mymousemove(e){
+    currentx=e.clientX-canvas.offsetLeft;
+    currenty=e.clientY-canvas.offsetTop;
+    if(mouseevent=="mousedown"){
+        ctx.beginPath();
+        ctx.strokeStyle="red";
+        ctx.linewidth=2;
+        ctx.moveTo(lastx,lasty);
+        ctx.lineTo(currentx,currenty);
+        ctx.stroke();
+    }
+    lastx=currentx;
+    lasty=currenty;
+}
+
